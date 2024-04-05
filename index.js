54d505ff3250fa2a122d68d7a714c287640cf6ae
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-04-05 20:45:12 +0300

    Define a function to find year of win

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function superbowlWin(record){
+    const everWon = record.find(element => element.result === 'W')
+    // console.log(everWon)
+    if (everWon){
+        return everWon.year;
+    }else{
+        return everWon;
+    }
+}
+
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+//   ]
+
+//   console.log(superbowlWin(record))
